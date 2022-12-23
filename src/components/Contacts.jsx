b239e504c01ca17ffc8164c852f56e80f096b8c4
Saksham Gupta <sakshamprem2003@gmail.com>
2022-12-24 03:29:13 +0530

    first commit

---
+++ src/components/Contacts.jsx
@@ -0,0 +1,186 @@
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
+import Logo from "../assets/logooo.png";
+import Logout from "./Logout";
+
+export default function Contacts({ contacts, changeChat }) {
+  const [currentUserName, setCurrentUserName] = useState(undefined);
+  const [currentUserImage, setCurrentUserImage] = useState(undefined);
+  const [currentSelected, setCurrentSelected] = useState(undefined);
+  useEffect(async () => {
+    const data = await JSON.parse(
+      localStorage.getItem(process.env.REACT_APP_LOCALHOST_KEY)
+    );
+    setCurrentUserName(data.username);
+    setCurrentUserImage(data.avatarImage);
+  }, []);
+  const changeCurrentChat = (index, contact) => {
+    setCurrentSelected(index);
+    changeChat(contact);
+  };
+  return (
+    <>
+      {currentUserImage && currentUserImage && (
+        <Container>
+          <div className="brand">
+            <img src={Logo} alt="logo" />
+            <h3>ToGather</h3>
+            
+              <Logout/>
+           
+
+          </div>
+          <div className="contacts">
+            {contacts.map((contact, index) => {
+              return (
+                <div
+                  key={contact._id}
+                  className={`contact ${
+                    index === currentSelected ? "selected" : ""
+                  }`}
+                  onClick={() => changeCurrentChat(index, contact)}
+                >
+                  <div className="avatar">
+                    <img
+                      src={`data:image/svg+xml;base64,${contact.avatarImage}`}
+                      alt=""
+                    />
+                  </div>
+                  <div className="username">
+                    <h3>{contact.username}</h3>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+          <div className="current-user">
+            <div className="avatar">
+              <img
+                src={`data:image/svg+xml;base64,${currentUserImage}`}
+                alt="avatar"
+              />
+            </div>
+            <div className="username">
+              <h2>{currentUserName}</h2>
+              
+            </div>
+           
+          </div>
+          
+        </Container>
+      )}
+    </>
+  );
+}
+const Container = styled.div`
+  display: grid;
+  grid-template-rows: 10% 75% 15%;
+  overflow: hidden;
+  background-color: #2f2d52;
+  border-left: 2px solid blue;
+  .brand {
+    display: flex;
+    align-items: center;
+    gap: 1rem;
+    justify-content: center;
+    img {
+      height: 2.5rem;
+      border-radius: 50%;
+      border: 2px solid blue;
+      
+        
+    }
+    h3 {
+      color: white;
+      
+      border-bottom: 2px solid blue;
+
+
+      //text-transform: uppercase;
+      
+    }
+  }
+  .contacts {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    overflow: auto;
+    gap: 0.8rem;
+    &::-webkit-scrollbar {
+      width: 0.2rem;
+      &-thumb {
+        background-color: blue;
+        width: 0.1rem;
+        border-radius: 1rem;
+      }
+    }
+   
+    .contact {
+      background-color: #ffffff34;
+      min-height: 0.1rem;
+      cursor: pointer;
+      width: 90%;
+      border-radius: 0.2rem;
+      padding: 0.4rem;
+      display: flex;
+      gap: 1rem;
+      align-items: center;
+      // transition: 0.5s ease-in-out;
+      &:hover {
+        background-color: black;
+        min-height: 4px;
+        transition: 0.1s linear;
+      }
+      .avatar {
+        img {
+          height: 3rem;
+          padding: 5px
+        }
+      }
+      .username {
+        h3 {
+          color: white;
+        }
+        
+      }
+    }
+    .selected {
+      background-color: blue;
+    }
+  }
+
+  .current-user {
+    background-color: #0d0d30;
+    display: flex;
+    justify-content: left;
+    align-items: center;
+    border-top: 2px solid blue;
+    gap: 2rem;
+    .avatar {
+      img {
+        height: 4.8rem;
+        max-inline-size: 100%;
+        padding: 10px;
+        &:hover{
+          height: 5.2rem;
+          
+          
+        }
+        
+      }
+    }
+    .username {
+      h2 {
+        color: white;
+      }
+    }
+    @media screen and (min-width: 480px) and (max-width: 720px) {
+      gap: 0.5rem;
+      .username {
+        h2 {
+          font-size: 1rem;
+        }
+      }
+    }
+  }
+`;
